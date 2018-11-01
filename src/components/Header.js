70737c6560c81a6c4a373b181738dde18b21428b
vasilis <vasiliss_b@hotmail.com>
2018-11-01 13:12:31 +0200

    add link to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = (props) => {
   return (
     <header className="header">
     <div className="header-title">
-        <h1>Responsive Pages Manager</h1>
+    <Link to={"/"}><h1>Responsive Pages Manager</h1></Link>
         </div>
       </header>
   );
